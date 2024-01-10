5ea04a3b2fde6e8d26f79bdfbb1a87a3e9552eb7
Rob Ramsay <rob.ramsay@ackama.com>
2024-01-11 01:47:26 +1300

    Add type and tweak char printing

--- life.go
+++ life.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"time"
+	"log"
 )
 
+type lifeBoard [3][3]byte;
+
+// func cellIsLive(cell byte) {
 func main() {
 	board := setup_board()
-
 	printBoard(board)
 
-	// ------
 	time.Sleep(2 * time.Second)
 
 	// mutate_board
@@ -19,11 +21,11 @@ func main() {
 	printBoard(board)
 }
 
-func setup_board() [3][3]byte  {
-	board := [3] [3]byte {
-		[3]byte{' ', ' ', '*'},
-		[3]byte{' ', '*', ' '},
-		[3]byte{'*', ' ', ' '},
+func setup_board() lifeBoard {
+	board := [3][3]byte {
+		[3]byte{'.', '.', '@'},
+		[3]byte{'.', '@', '.'},
+		[3]byte{'@', '.', '.'},
 	}
 
 	return board
@@ -33,14 +35,14 @@ func clearScreen() {
 	fmt.Printf("\033[H\033[2J")
 }
 
-func printBoard(board [3][3]byte) {
+func printBoard(board lifeBoard) {
 	clearScreen()
 
 	fmt.Printf("\n------\n")
 
     for i := 0; i <= 2; i++ {
 		for j := 0; j <= 2; j++ {
-			fmt.Printf("%c", board[i][j])
+			fmt.Printf(" %c", board[i][j])
 		}
 		fmt.Printf("\n")
     }
