fbd7f50d947de0b265c009dc13bec1f19a2ccb6a
Rob Ramsay <rob.ramsay@ackama.com>
2024-01-11 00:40:19 +1300

    Refactoring into function'

--- life.go
+++ life.go
@@ -6,47 +6,44 @@ import (
 )
 
 func main() {
-	// setup_board
-	//
+	board := setup_board()
 
-	board := [] [3]byte {
-		[3]byte{' ', ' ', ' '},
-		[3]byte{' ', ' ', ' '},
-		[3]byte{' ', ' ', ' '},
-	}
-
-	// create_first_pattern
-	//
-
-	board = [] [3]byte {
-		[3]byte{' ', ' ', '*'},
-		[3]byte{' ', '*', ' '},
-		[3]byte{'*', ' ', ' '},
-	}
-
-	// print board
-	//
-	fmt.Printf("\n\n------\n")
-    for i := 0; i <= 2; i++ {
-		for j := 0; j <= 2; j++ {
-			fmt.Printf("%c", board[i][j])
-		}
-		fmt.Printf("\n")
-    }
+	printBoard(board)
 
 	// ------
 	time.Sleep(2 * time.Second)
 
 	// mutate_board
 	//
+	board[0][0] = '*'
+	printBoard(board)
+}
 
-	// print board
-	//
-	fmt.Printf("\n\n------\n")
+func setup_board() [3][3]byte  {
+	board := [3] [3]byte {
+		[3]byte{' ', ' ', '*'},
+		[3]byte{' ', '*', ' '},
+		[3]byte{'*', ' ', ' '},
+	}
+
+	return board
+}
+
+func clearScreen() {
+	fmt.Printf("\033[H\033[2J")
+}
+
+func printBoard(board [3][3]byte) {
+	clearScreen()
+
+	fmt.Printf("\n------\n")
+
     for i := 0; i <= 2; i++ {
 		for j := 0; j <= 2; j++ {
 			fmt.Printf("%c", board[i][j])
 		}
 		fmt.Printf("\n")
     }
+
+	fmt.Printf("------\n")
 }
