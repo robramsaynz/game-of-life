eb0217a8de143acf6ff0345efca31eca4702b75c
Rob Ramsay <rob.ramsay@ackama.com>
2024-01-16 15:01:30 +1300

    Cleanup tests

--- life_test.go
+++ life_test.go
@@ -1,18 +1,8 @@
 package main
 
 import (
-	// "fmt"
     "testing"
 )
-
-// func TestIntMinBasic
-//     ans := IntMin(2, -2)
-//     if ans != -2 {
-// t.Error* will report test failures but continue executing the test. t.Fatal* will report test failures and stop the test immediately.
-//         t.Errorf("IntMin(2, -2) = %d; want -2", ans)
-//     }
-// }
-
 
 // returns an 0 or 1 depending on whether this adds to the
 // count of alive neighbours
